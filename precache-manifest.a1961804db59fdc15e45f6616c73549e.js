f39493013b34c0608c0fe8fa1f7ee8e8ac767660
andolino <dondonpentavia@gmail.com>
2018-10-08 14:15:18 +0800

    Updates

---
+++ precache-manifest.a1961804db59fdc15e45f6616c73549e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fe9c4630df9bbc8d698",
+    "url": "/scc-react-exercise/static/js/runtime~main.7fe9c463.js"
+  },
+  {
+    "revision": "233ec6bfdf40633321ae",
+    "url": "/scc-react-exercise/static/js/main.233ec6bf.chunk.js"
+  },
+  {
+    "revision": "f4d10aee93265461729a",
+    "url": "/scc-react-exercise/static/js/1.f4d10aee.chunk.js"
+  },
+  {
+    "revision": "2d80e3ec4d69be743cb1bed10e245187",
+    "url": "/scc-react-exercise/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.04dae7169de5c7fed106e29ea6611986.js"
+  "/scc-react-exercise/precache-manifest.a1961804db59fdc15e45f6616c73549e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/scc-react-exercise/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
